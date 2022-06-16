d947a12783fe0cf1364a15905720475d08d8cf7f
Anthony P. Tran <atranp@gmail.com>
2022-06-16 18:55:32 -0400

    Create first route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ import express from "express";
 // instantiate app
 const app = express();
 
+// routes
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the app'})
+})
+
 //listen for requests
 app.listen(4000, ()=> {
     console.log('Listening on port 4000')
