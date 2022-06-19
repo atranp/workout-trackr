fbeccb23b62fd00541f9f75ca6a6bad4274dc6aa
Anthony P. Tran <atranp@gmail.com>
2022-06-19 14:50:59 -0400

    Setup mongoose, and connect to db

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import router from "./routes/workout.js";
-import 'dotenv/config';
+import mongoose from "mongoose";
+import "dotenv/config";
 
 // instantiate app
 const app = express();
 
 //middlewares
-app.use(express.json()) // Parses incoming JSON requests and passes to req
+app.use(express.json()); // Parses incoming JSON requests and passes to req
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next();
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', router)
+app.use("/api/workouts", router);
 
-//listen for requests
-app.listen(process.env.PORT, ()=> {
-    console.log('Listening on port', process.env.PORT)
-})
+// connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
