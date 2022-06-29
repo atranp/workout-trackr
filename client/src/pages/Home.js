bfe910518407437c1b0adaa0928a946afc0e258f
Anthony P. Tran <atranp@gmail.com>
2022-06-28 20:03:40 -0400

    add styling for workout post form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import WorkoutDetails from '../components/WorkoutDetails/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm/WorkoutForm';
 import './home.css'
 
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null);
+  const [workouts, setWorkouts] = useState(null);
 
-    useEffect(() => {
-      const fetchWorkouts = async () => {
-        const response = await fetch('/api/workouts')
-        const json = await response.json();
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json();
 
-        if (response.ok) {
-            setWorkouts(json)
-        }
+      if (response.ok) {
+        setWorkouts(json)
       }
+    }
 
-      fetchWorkouts();
-    }, [])
+    fetchWorkouts();
+  }, [])
 
 
   return (
     <main>
-    <div className="home">
+      <div className="home">
         <div className="workouts">
-        {workouts && workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout}/>
-        ))}
+          {workouts && workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
         </div>
-    </div>
+        <WorkoutForm />
+      </div>
     </main>
   )
 }
