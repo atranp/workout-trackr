2de56f37e1d974d7ff77622f30b3a1b0ae8560f8
Anthony P. Tran <atranp@gmail.com>
2022-07-01 14:29:10 -0400

    Build out DELETE logic in Context

--- client/src/components/WorkoutDetails/WorkoutDetails.js
+++ client/src/components/WorkoutDetails/WorkoutDetails.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import './workoutDetails.css'
+import React from "react";
+import { useWorkoutsContext } from "../../hooks/useWorkoutsContext";
+import "./workoutDetails.css";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workoutDetails">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (lb): </strong>{workout.load}</p>
-        <p><strong>Reps: </strong> {workout.reps}</p>
-        <p>{workout.createdAt}</p>
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load (lb): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong> {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
