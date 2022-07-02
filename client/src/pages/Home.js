cda32e7b88a40cbc1c1359d0c7e46533a5574166
Anthony P. Tran <atranp@gmail.com>
2022-07-02 14:04:22 -0400

    add appropriate headers to handle API requests

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,39 +1,40 @@
-import React, { useEffect } from 'react'
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
-import WorkoutDetails from '../components/WorkoutDetails/WorkoutDetails';
-import WorkoutForm from '../components/WorkoutForm/WorkoutForm';
-import './home.css'
-
+import React, { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import WorkoutDetails from "../components/WorkoutDetails/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm/WorkoutForm";
+import "./home.css";
 
 const Home = () => {
-  const {workouts, dispatch} = useWorkoutsContext();
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
+      const response = await fetch(
+        "https://workouttrackr.herokuapp.com/api/workouts"
+      );
       const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_WORKOUTS', payload: json})
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
-    }
+    };
 
     fetchWorkouts();
-  }, [dispatch])
-
+  }, [dispatch]);
 
   return (
     <main>
       <div className="home">
         <div className="workouts">
-          {workouts && workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
-          ))}
+          {workouts &&
+            workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout} />
+            ))}
         </div>
         <WorkoutForm />
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
