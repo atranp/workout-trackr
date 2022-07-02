4ecc494b9ce5a6c22faf70ecc0cadcb6079c9fc3
Anthony P. Tran <atranp@gmail.com>
2022-07-02 13:12:19 -0400

    degubbing heroku

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use("/api/workouts", router);
 
 // connect to DB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     //listen for requests
     app.listen(process.env.PORT, () => {
